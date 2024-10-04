4d3e5f082285ae4a0cd0354d01d531ff33aaa551
Kristof Van Der Haeghen <k.van.der.haeghen.aleacsys@napoleongames.be>
2024-10-04 16:34:38 +0200

    Adding zoom in/out functionality + Making button & select smaller and creating a good disabled state + Adding move functionality for the spine objects with a button to start and stop it.

--- src/views/game.ts
+++ src/views/game.ts
@@ -4,15 +4,24 @@ import { createTickEventHandler, ITickEventHandler, ResolvablePromise } from "@g
 import { SkeletonData, Spine } from '@pixi-spine/all-4.1';
 import { AtlasAttachmentLoader, SkeletonJson } from '@pixi-spine/runtime-4.1';
 import * as pixiSpine from 'pixi-spine';
-
+import * as newSpine from '@esotericsoftware/spine-pixi';
 import { SpineObject } from '../models/spineObject';
 
 export class SpineMachine {
     private app?: PIXI.Application<HTMLCanvasElement>;
     private tickEventHandler: ITickEventHandler = createTickEventHandler();
 
+    private objectContainer: PIXI.Container = new PIXI.Container();
+    private pinchCenter: { x: number, y: number } = { x: 0, y: 0 };
+    private pinchDistance: number = 0;
+
+    private addButton: HTMLButtonElement;
+    private moveButton: HTMLButtonElement;
+    private spineListMenu: HTMLSelectElement;
+    private animationListMenu: HTMLSelectElement;
+
+    private static SPINE_ADD_COUNT = 100;
     private spineObjects: Spine[] = [];
-    private static SPINE_COUNT = 100;
     private spineList: SpineObject[] = [{
         name: "Diamond",
         isBase64: false,
@@ -27,11 +36,18 @@ export class SpineMachine {
         texturePath: "assets/theseus.tex1.png"
     }];
     private animationList: SpineObject[] = [];
+
     private selectedSpineIndex: number = 0;
     private selectedAnimIndex: number = 0;
+    private canMove: boolean = false;
+
+    private webglContext: WebGLRenderingContext | WebGL2RenderingContext | null | undefined;
+    private webglDrawCalls: number = 0;
+    private realDrawElements: Function;
+    private realWebGLClear: Function;
 
     private readonly maxDropSize = 3;
-    private dropZonePromise = new ResolvablePromise<void>();
+    private readonly moveSpeed = 500;
 
     constructor() {
         this.app = new PIXI.Application<HTMLCanvasElement>({ 
@@ -40,35 +56,112 @@ export class SpineMachine {
             height: 600, 
         });
         document.body.appendChild(this.app.view);
+        this.app?.stage?.addChild(this.objectContainer);
 
-        this.addItemsToList('spine-list', this.spineList);
+        this.initWebGLContext();
         this.initElements();
         this.initDropZone();
     }
 
+    private initWebGLContext(): void {
+        let gl = this.app?.view.getContext('webgl');
+        if (!gl) {
+             gl =  this.app?.view.getContext('webgl2');
+        }
+        if (gl) {
+            // @ts-ignore
+            this.realDrawElements = gl.__proto__.drawElements;
+            // @ts-ignore
+            this.realWebGLClear = gl.__proto__.clear;
+            // @ts-ignore
+            gl.__proto__.drawElements = this.fakeDrawElements.bind(this);
+            // @ts-ignore
+            gl.__proto__.clear = this.fakeWebGLClear.bind(this);
+        }
+        this.webglContext = gl;
+    }
+
     private initElements(): void {
-        const addButton = document.getElementById('add');
-        addButton?.addEventListener('click', async () => {
+        this.addItemsToList('spine-list', this.spineList);
+
+        this.addButton = document.getElementById('add') as HTMLButtonElement;
+        this.addButton?.addEventListener('click', async () => {
             await this.addSpineObjects();
+            this.moveButton.disabled = false;
+        });
+
+        this.moveButton = document.getElementById('move') as HTMLButtonElement;
+        this.moveButton?.addEventListener('click', async () => {
+            this.canMove = !this.canMove;
+            this.moveButton.innerText = this.canMove ? 'Stop' : 'Move';
         });
 
         addEventListener('load', () => {
             void this.loadSpineAnimations();
         })
-        const spineListMenu = document.getElementById('spine-list') as HTMLSelectElement;
-        spineListMenu.addEventListener('change', () => {
-            this.resetSpineObjects();
 
-            this.selectedSpineIndex = spineListMenu.selectedIndex;
+        this.app?.view.addEventListener('wheel', (event) => {
+            const scale = 1 - (event.deltaY * 0.001);
+            const screenCenterX = (this.app!.view.width * 0.5);
+            const screenCenterY = (this.app!.view.height * 0.5);
+            this.objectContainer.position.x = this.objectContainer.position.x + (screenCenterX - this.objectContainer.position.x) * (1 - scale);
+            this.objectContainer.position.y = this.objectContainer.position.y + (screenCenterY - this.objectContainer.position.y) * (1 - scale);
+            this.objectContainer.scale.x *= scale;
+            this.objectContainer.scale.y *= scale;
+        });
+
+        
+        this.app?.view.addEventListener('touchstart', (event) => {
+            this.pinchCenter = { x: event.touches[0].clientX, y: event.touches[0].clientY };
+            this.pinchDistance = Math.hypot(event.touches[0].clientX - event.touches[1].clientX, event.touches[0].clientY - event.touches[1].clientY);
+        });
+
+        this.app?.view.addEventListener('touchmove', (event) => {
+            const distance = Math.hypot(event.touches[0].clientX - event.touches[1].clientX, event.touches[0].clientY - event.touches[1].clientY);
+            const scale = distance / this.pinchDistance;
+            const screenCenterX = (this.app!.view.width * 0.5);
+            const screenCenterY = (this.app!.view.height * 0.5);
+            this.objectContainer.position.x = this.pinchCenter.x + (screenCenterX - this.pinchCenter.x) * scale;
+            this.objectContainer.position.y = this.pinchCenter.y + (screenCenterY - this.pinchCenter.y) * scale;
+            this.objectContainer.scale.x *= scale;
+            this.objectContainer.scale.y *= scale;
+            this.pinchDistance = distance;
+        });
+
+
+        this.spineListMenu = document.getElementById('spine-list') as HTMLSelectElement;
+        this.spineListMenu?.addEventListener('change', () => {
+            this.reset();
+            this.selectedAnimIndex = 0;
+            this.selectedSpineIndex = this.spineListMenu.selectedIndex;
             void this.loadSpineAnimations();
         });
 
-        const animListMenu = document.getElementById('animation-list') as HTMLSelectElement;
-        animListMenu.addEventListener('change', () => {
-            this.selectedAnimIndex = animListMenu.selectedIndex;
+        this.animationListMenu = document.getElementById('animation-list') as HTMLSelectElement;
+        this.animationListMenu?.addEventListener('change', () => {
+            this.selectedAnimIndex = this.animationListMenu.selectedIndex;
             this.restartAnimations();
         });
 
+    }
+
+    private reset(): void {
+        this.moveButton.disabled = true;
+        this.resetSpineObjects();
+        this.resetObjectContainer();
+    }
+    private resetSpineObjects(): void {
+        this.spineObjects.forEach((spineObject) => {
+            spineObject.destroy();
+        });
+        this.spineObjects.splice(0);
+        this.updateCounter();
+    }
+    private resetObjectContainer(): void {
+        this.objectContainer.position.x = 0;
+        this.objectContainer.position.y = 0;
+        this.objectContainer.scale.x = 1;
+        this.objectContainer.scale.y = 1;
     }
 
     private addItemsToList(elementId: string, items: SpineObject[]): void {
@@ -93,8 +186,7 @@ export class SpineMachine {
         // Get the element
         const dropZone = document.getElementById('drop-zone');
 
-        // Add event listeners
-        // To avoid flickering of the canvas, use 'dragover' on the body and 'dragleave' & 'drop' on the drop zone
+        // Add event listeners. To avoid flickering of the canvas, use 'dragover' on the body and 'dragleave' & 'drop' on the drop zone
         document.addEventListener('dragover', (event) => {
             // Prevent default behavior
             event.preventDefault();
@@ -110,7 +202,6 @@ export class SpineMachine {
     }
 
     public async dropHandler(event: DragEvent): Promise<void> {
-
             // Prevent default behavior
             event.preventDefault();
             this.dragHandler(false);
@@ -131,48 +222,51 @@ export class SpineMachine {
                     jsonPath: '',
                     texturePath: '',
                 };
-
                 // Loop through the files
                 for (let i = 0; i < files.length; i++) {
-                    // Create resolvablePromise to wait for drop to finish loading
-                    const resolvePromise = new ResolvablePromise<void>();
-                    const file = files[i];
-                    // Check the file type
-                    if (file.type === 'image/png' || file.type === 'image/jpeg' || file.type === 'image/jpg' || file.name.endsWith('.atlas') || file.name.endsWith('.atlas.txt') || file.name.endsWith('.json')) {
-                        // Create file reader
-                        const reader = new FileReader();
-                        // Read the file as url
-                        reader.readAsDataURL(file);
-
-                        reader.onload = () => {
-                            // Do something with the file contents
-                            const fileContents = reader.result;
-                            console.log(`Received file: ${file.name}`);
-                            spineObject.name = file.name.split('.').shift() || '';
-                            if (file.name.endsWith('.atlas') || file.name.endsWith('.atlas.txt')) {
-                                spineObject.atlasPath = fileContents as string;
-                            } else if (file.name.endsWith('.json')) {
-                                spineObject.jsonPath = fileContents as string;
-                            } else if (file.type === 'image/png' || file.type === 'image/jpeg' || file.type === 'image/jpg') {
-                                spineObject.texturePath = fileContents as string;
-                            } else {
-                                const dotIndex = file.name.indexOf('.');
-                                const extension = file.name.substring(dotIndex);
-                                console.warn(`Unsupported file extension: ${extension}`);
-                            }
-                            resolvePromise.resolve();
-                        };
-                    } else {
-                        console.warn(`Unsupported file type: ${file.type}`);
-                        resolvePromise.resolve();
-                    }
-                    await resolvePromise.promise;
+                    await this.loadDroppedSpineFiles(files[i], spineObject);
                 }
                 
                 this.spineList.push(spineObject);
                 this.addItemsToList('spine-list', this.spineList);
                 console.log('Spine object added:', spineObject);
             }
+    }
+
+    private loadDroppedSpineFiles(file: File, spineObject: SpineObject): Promise<void> {
+        // Create resolvablePromise to wait for drop to finish loading
+        const resolvePromise = new ResolvablePromise<void>();
+
+        // Check the file type
+        if (file.type === 'image/png' || file.type === 'image/jpeg' || file.type === 'image/jpg' || file.name.endsWith('.atlas') || file.name.endsWith('.atlas.txt') || file.name.endsWith('.json')) {
+            // Create file reader
+            const reader = new FileReader();
+            // Read the file as url
+            reader.readAsDataURL(file);
+
+            reader.onload = () => {
+                // Do something with the file contents
+                const fileContents = reader.result;
+                console.log(`Received file: ${file.name}`);
+                spineObject.name = file.name.split('.').shift() || '';
+                if (file.name.endsWith('.atlas') || file.name.endsWith('.atlas.txt')) {
+                    spineObject.atlasPath = fileContents as string;
+                } else if (file.name.endsWith('.json')) {
+                    spineObject.jsonPath = fileContents as string;
+                } else if (file.type === 'image/png' || file.type === 'image/jpeg' || file.type === 'image/jpg') {
+                    spineObject.texturePath = fileContents as string;
+                } else {
+                    const dotIndex = file.name.indexOf('.');
+                    const extension = file.name.substring(dotIndex);
+                    console.warn(`Unsupported file extension: ${extension}`);
+                }
+                resolvePromise.resolve();
+            };
+        } else {
+            console.warn(`Unsupported file type: ${file.type}`);
+            resolvePromise.resolve();
+        }
+        return resolvePromise.promise;
     }
 
     public init(): void {
@@ -200,7 +294,7 @@ export class SpineMachine {
 
     private addSpineObjects(): Promise<void> {
         return this.loadSpineObject().then((data) => {
-            for (let i = 0; i < SpineMachine.SPINE_COUNT; i++) {
+            for (let i = 0; i < SpineMachine.SPINE_ADD_COUNT; i++) {
                 this.createSpineObject(data.spineData); // Create a new Spine animation
             }
             this.updateCounter();
@@ -275,7 +369,8 @@ export class SpineMachine {
             // Randomly position the spine animation within the given range
             spineObject.x = 50 + Math.random() * 700; // Random x between 0 and 700
             spineObject.y = 25 + Math.random() * 500; // Random y between 0 and 500
-            spineObject.scale.set(0.2, 0.2);
+            spineObject.scale.set(0.2);
+
 
             // Start animation
             if (this.animationList.length > 0) {
@@ -283,7 +378,7 @@ export class SpineMachine {
             }
 
             // Add the spine character to the stage
-            this.app?.stage.addChild(spineObject);
+            this.objectContainer.addChild(spineObject);
             this.spineObjects.push(spineObject);
         }
     }
@@ -294,13 +389,7 @@ export class SpineMachine {
         });
     }
 
-    private resetSpineObjects(): void {
-        this.spineObjects.forEach((spineObject) => {
-            spineObject.destroy();
-        });
-        this.spineObjects.splice(0);
-        this.updateCounter();
-    }
+
 
     private createLogo(): void {
         const logo = new PIXI.Sprite(PIXI.Texture.from('https://i.imgur.com/UjL9rZJ.png'));
@@ -308,7 +397,7 @@ export class SpineMachine {
         logo.anchor.set(0.5);
         logo.x = 400;
         logo.y = 550;
-        this.app?.stage?.addChild(logo);
+        this.app?.stage.addChild(logo);
       }
 
     private lastTime = Date.now();
@@ -319,8 +408,38 @@ export class SpineMachine {
             this.lastTime = currentTime;
             this.tickEventHandler.updateEvents(deltaTime * 0.001);
             requestAnimationFrame(updater);
+            this.moveSpineObjects(deltaTime * 0.001);
         };
         updater();
     }
 
+    public moveSpineObjects(deltaTime: number): void {
+        if (this.canMove) {
+            const appHeight = this.app!.renderer.height;
+            for (let i = 0; i < this.spineObjects.length; i++) {
+                const spineObject = this.spineObjects[i];
+                spineObject.y += this.moveSpeed * deltaTime;
+                if (spineObject.y > appHeight) {
+                    spineObject.y = 0;
+                }
+            }
+        }
+    }
+
+    private updateDrawCalls(count: number): void {
+        const drawCalls: HTMLElement = document.getElementById('draw-calls')!;
+        drawCalls.innerText = `${count} Draw Calls`;
+    }
+    private fakeDrawElements(mode: number, count: number, type: number, offset: number): void {
+        this.webglDrawCalls++;
+        this.realDrawElements.call(this.webglContext, mode, count, type, offset);
+    }
+    private fakeWebGLClear(bitmask: number): void {
+        if (bitmask == 16640) {
+            this.updateDrawCalls(this.webglDrawCalls);
+            this.webglDrawCalls = 0;
+        }
+
+        this.realWebGLClear.call(this.webglContext, bitmask);
+    }
 }
